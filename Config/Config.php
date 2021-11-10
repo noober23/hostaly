bb733ec74f73b0a6101f566e8ab891cc27cc5d40
noober23 <decartsd@hotmail.com>
2021-11-09 20:06:26 -0400

    second commits

--- Config/Config.php
+++ Config/Config.php
@@ -1,9 +1,9 @@
 <?php
 
-const BASE_URL="heroku_4e8b11e241cef3a";
+const BASE_URL="hostaly.herokuapp.com";
 
-const DB_HOST="localhost";
-const DB_NAME="hostal";
+const DB_HOST="us-cdbr-east-04.cleardb.com";
+const DB_NAME="heroku_4e8b11e241cef3a";
 const DB_USER="bd011ee28cbd6e";
 const DB_PASS= "fa2007dd";
 //mysql://bd011ee28cbd6e:fa2007dd@us-cdbr-east-04.cleardb.com/heroku_4e8b11e241cef3a?reconnect=true
