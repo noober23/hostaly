bf7ce1db79ee005529d51835b29b88d94e1c4fc6
noober23 <decartsd@hotmail.com>
2022-04-27 21:18:47 -0400

    reverse commit

--- Config/Config.php
+++ Config/Config.php
@@ -1,12 +1,12 @@
 <?php
 
-const BASE_URL="hostaly.herokuapp.com";
+const BASE_URL="localhost";
 
-const DB_HOST="us-cdbr-east-04.cleardb.com";
-const DB_NAME="heroku_4e8b11e241cef3a";
-const DB_USER="bd011ee28cbd6e";
-const DB_PASS= "fa2007dd";
-//mysql://bd011ee28cbd6e:fa2007dd@us-cdbr-east-04.cleardb.com/heroku_4e8b11e241cef3a?reconnect=true
+const DB_HOST="DB";
+const DB_NAME="db";
+const DB_USER="root";
+const DB_PASS= "";
+
 const DB_CHARSET="charset=utf8";
 
 const SGBD="mysql:host=".DB_HOST.";dbname=".DB_NAME;
